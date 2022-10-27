89e7c9d4da61be0118158b105de9d51609349dd0
Corina <morarik8@gmail.com>
2022-10-27 14:32:08 +0300

    Calculator done

--- script/script.js
+++ script/script.js
@@ -1,14 +1,34 @@
-const h3 = document.querySelector(".result");
-const val1 = +prompt("Enter first number");
-const val2 = +prompt("Enter second number");
+const buttons = document.querySelectorAll("input[type = button]")
+console.log(buttons)
+const input = document.querySelector('input')
+console.log(input)
 
-const operator = prompt("Choose operator");
-if (operator === "+") {
-  h3.textContent = val1 + val2;
-} else if (operator === "-") {
-  h3.textContent = val1 - val2;
-} else if (operator === "*") {
-  h3.textContent = val1 * val2;
-} else if (operator === "/") {
-  h3.textContent = val1 / val2;
+
+const display = (character) => {
+    input.value += `${character}`
 }
+
+const solve = () => {
+    const x = input.value;
+    const y = eval(x);
+    return input.value = `${y}`
+}
+
+const clear = () => {
+    return input.value = ""
+}
+buttons.forEach(item =>{
+    item.addEventListener('click',()=>{
+        const value = item.value;
+        if(value === "="){
+            solve()
+        }else if(value === "c"){
+            clear();
+        }else{
+            display(value)
+        }
+
+
+    })
+
+})
